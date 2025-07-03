9491f02b5ace858239697b308007030517bb5bdf
Andrea Goisis <andrea.goisis@partner.bmwgroup.com>
2025-07-03 13:33:30 +0200

    Implement GetExchangeRatesAsync logic

--- jobs/Backend/Task/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateProvider.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Globalization;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
@@ -14,7 +17,54 @@ namespace ExchangeRateUpdater
         /// </summary>
         public async Task<IEnumerable<ExchangeRate>> GetExchangeRatesAsync(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            string baseUrl = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/selected.txt";
+
+            var exchangeRates = new List<ExchangeRate>();
+
+            var now = DateTime.Today;
+            string from = now.AddMonths(-1).ToString("dd.MM.yyyy"); //last 30 days rates
+            string to = now.ToString("dd.MM.yyyy");
+            Currency sourceCurrency = new Currency("CZK");
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                foreach (var currency in currencies)
+                {
+
+                    string url = $"{baseUrl}?from={from}&to={to}&currency={currency.Code}&format=text"; 
+
+                    try
+                    {
+                        string resp = await httpClient.GetStringAsync(url);
+
+                        if (string.IsNullOrWhiteSpace(resp))
+                            continue;
+
+                        Currency targetCurrency = new Currency(currency.Code);
+
+                        var rawData = resp.Split(new[] { '\n', '|' }, StringSplitOptions.RemoveEmptyEntries); 
+
+                        string amountString = rawData.Single(x => x.Contains("Amount")).Split(':').Last().Trim(); //Format is "Amount: 100"
+
+                        decimal value = decimal.Parse(rawData.Last(), CultureInfo.InvariantCulture) / int.Parse(amountString); //rawData.Last() is most recent value
+
+                        exchangeRates.Add(new ExchangeRate(sourceCurrency, targetCurrency, value));
+
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine($"HTTP error for {currency.Code}: {ex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Generic error for {currency.Code}: {ex.Message}");
+                    }
+
+                }
+
+            }
+
+            return exchangeRates;
         }
     }
 }
